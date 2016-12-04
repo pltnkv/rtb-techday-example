ec7edf5a35e7c7440eae017718d7e7f132e55486
pltnkv <pltnkv@gmail.com>
2016-12-04 09:16:15 +0500

    add stylelinter and prod mode

--- webpack.config.js
+++ webpack.config.js
@@ -2,92 +2,105 @@ var path = require('path');
 var webpack = require('webpack');
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var StyleLintPlugin = require('stylelint-webpack-plugin');
 var autoprefixer = require('autoprefixer');
 
-//пока не используется, понадобиться для production-сборки
 var hashFiles = false
+
 function addHash(template, hash) {
-    return hashFiles ? template.replace(/\.[^.]+$/, '.[' + hash + ']$&') : template
+	return hashFiles ? template.replace(/\.[^.]+$/, '.[' + hash + ']$&') : template
 }
 
 function getCommonConfig() {
-    return {
-        entry: './src/index.ts',
-        output: {
-            path: path.resolve('./target'),
-            filename: addHash('[name].js', 'chunkhash')
-        },
-        resolve: {
-            root: [path.resolve('./src'), path.resolve('./static')],
-            modulesDirectories: ['node_modules'],
-            extensions: ['', '.ts', '.tsx', '.js'],
-        },
+	return {
+		entry: './src/index.ts',
+		output: {
+			path: path.resolve('./target'),
+			filename: addHash('[name].js', 'chunkhash')
+		},
+		resolve: {
+			root: [path.resolve('./src'), path.resolve('./static')],
+			modulesDirectories: ['node_modules'],
+			extensions: ['', '.ts', '.tsx', '.js'],
+		},
 
-        module: {
-            preLoaders: [{
-                test: /\.tsx?$/,
-                loader: 'tslint-loader'
-            }],
-            loaders: [{
-                test: /\.tsx?$/,
-                include: path.resolve('./src'),
-                loader: 'ts-loader'
-            }, {
-                test: /\.(png|jpg|gif|ttf|eot|woff|woff2)$/,
-                loader: addHash('file?name=[path][name].[ext]', 'hash:6')
-            }]
-        },
+		module: {
+			preLoaders: [{
+				test: /\.tsx?$/,
+				loader: 'tslint-loader'
+			}],
+			loaders: [{
+				test: /\.tsx?$/,
+				include: path.resolve('./src'),
+				loader: 'ts-loader'
+			}, {
+				test: /\.(png|jpg|gif|ttf|eot|woff|woff2)$/,
+				loader: addHash('file?name=[path][name].[ext]', 'hash:6')
+			}]
+		},
 
-        plugins: [
-            new webpack.NoErrorsPlugin(),
-            new HtmlWebpackPlugin({
-                filename: 'app.html',
-                template: './static/app.html'
-            })
-        ],
+		plugins: [
+			new webpack.NoErrorsPlugin(),
+			new StyleLintPlugin({
+				configFile: '.stylelintrc',
+				files: ['**/*.less']
+			}),
+			new HtmlWebpackPlugin({
+				filename: 'app.html',
+				template: './static/app.html'
+			})
+		],
 
-        ts: {
-            compilerOptions: {
-                target: 'ES5',
-                jsx: 'react',
-                sourceMap: true
-            }
-        },
+		ts: {
+			compilerOptions: {
+				target: 'ES5',
+				jsx: 'react',
+				sourceMap: true
+			}
+		},
 
-        postcss: function() {
-            return [autoprefixer]
-        }
-    }
+		postcss: function() {
+			return [autoprefixer]
+		}
+	}
 }
 
-function addCssLoaders(config, devMode) {
-    if (devMode) {
-        config.module.loaders.push({
-            test: /\.css$/,
-            loader: 'style!css!postcss'
-        }, {
-            test: /\.less$/,
-            loader: 'style!css!postcss!less'
-        })
-    } else {
-		//пока не используется
-        config.module.loaders.push({
-            test: /\.css$/,
-            loader: ExtractTextPlugin.extract('css!postcss')
-        }, {
-            test: /\.less$/,
-            loader: ExtractTextPlugin.extract('style', 'css!postcss!less', {
-                publicPath: ''
-            })
-        })
-    }
+function addDevCssLoaders(config) {
+	config.module.loaders.push({
+		test: /\.css$/,
+		loader: 'style!css!postcss'
+	}, {
+		test: /\.less$/,
+		loader: 'style!css!postcss!less'
+	})
+}
+
+function addProdCssLoaders(config) {
+	config.module.loaders.push({
+		test: /\.css$/,
+		loader: ExtractTextPlugin.extract('css!postcss')
+	}, {
+		test: /\.less$/,
+		loader: ExtractTextPlugin.extract('style', 'css!postcss!less', {
+			publicPath: ''
+		})
+	})
 }
 
 function getDevelopmentConfig() {
-    hashFiles = false
-    var config = getCommonConfig()
-    addCssLoaders(config, true)
-    return config
+	var config = getCommonConfig()
+	addDevCssLoaders(config)
+	return config
 }
 
-module.exports = getDevelopmentConfig()
+function getProductionConfig() {
+    hashFiles = true
+	var config = getCommonConfig()
+	addProdCssLoaders(config)
+    config.plugins.push(new ExtractTextPlugin("styles.css")) //todo use addHash()
+	return config
+}
+
+
+// module.exports = getDevelopmentConfig()
+module.exports = getProductionConfig()
